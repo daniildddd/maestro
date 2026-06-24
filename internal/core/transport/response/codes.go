5ba8836cd189b04909331f774cb62a5d1a0c3fdf
Lyapunov Daniil <danyalyapunov@gmail.com>
2026-06-25 01:43:09 +0300

    feat(response): add error code constants

---
+++ internal/core/transport/response/codes.go
@@ -0,0 +1,8 @@
+package response
+
+const (
+	CodeInvalidCredentials  = "INVALID_CREDENTIALS"
+	CodeInvalidRefreshToken = "INVALID_REFRESH_TOKEN"
+	CodeInvalidRequestBody  = "INVALID_REQUEST_BODY"
+	CodeInternal            = "INTERNAL"
+)

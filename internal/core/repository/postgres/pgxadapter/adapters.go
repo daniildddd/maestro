6ff22cf84b7d056b360e678c08178b06dcd3b7cb
Lyapunov Daniil <danyalyapunov@gmail.com>
2026-07-05 19:26:39 +0300

    feat(pgxadapter): add pool and rows adapters with error mapping

---
+++ internal/core/repository/postgres/pgxadapter/adapters.go
@@ -0,0 +1,106 @@
+package pgxadapter
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	core_postgres_pool "github.com/daniildddd/maestro/internal/core/repository/postgres"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgconn"
+)
+
+type pgxRows struct {
+	pgx.Rows
+}
+
+func (r pgxRows) Scan(dest ...any) error {
+	err := r.Rows.Scan(dest...)
+	if err != nil {
+		return mapErrors(err)
+	}
+
+	return nil
+}
+
+func (r pgxRows) Err() error {
+	err := r.Rows.Err()
+	if err != nil {
+		return mapErrors(err)
+	}
+
+	return nil
+}
+
+type pgxRow struct {
+	pgx.Row
+}
+
+func (r pgxRow) Scan(dest ...any) error {
+	err := r.Row.Scan(dest...)
+	if err != nil {
+		return mapErrors(err)
+	}
+
+	return nil
+}
+
+type pgxCommandTag struct {
+	pgconn.CommandTag
+}
+
+func mapErrors(err error) error {
+	if err == nil {
+		return nil
+	}
+
+	if errors.Is(err, pgx.ErrNoRows) {
+		return core_postgres_pool.ErrNoRows
+	}
+
+	const (
+		pgxForeignKeyViolation = "23503"
+		pgxUniqueViolation     = "23505"
+		pgxCheckViolation      = "23514"
+		pgxDeadlockDetected    = "40P01"
+	)
+
+	var pgErr *pgconn.PgError
+	if errors.As(err, &pgErr) {
+		switch pgErr.Code {
+		case pgxForeignKeyViolation:
+			return fmt.Errorf(
+				"%w: %w",
+				err,
+				core_postgres_pool.ErrViolatesForeignKey,
+			)
+
+		case pgxUniqueViolation:
+			return fmt.Errorf(
+				"%w: %w",
+				err,
+				core_postgres_pool.ErrDuplicate,
+			)
+
+		case pgxCheckViolation:
+			return fmt.Errorf(
+				"%w: %w",
+				err,
+				core_postgres_pool.ErrValidation,
+			)
+
+		case pgxDeadlockDetected:
+			return fmt.Errorf(
+				"%w: %w",
+				err,
+				core_postgres_pool.ErrDeadlock,
+			)
+		}
+	}
+
+	if errors.Is(err, context.Canceled) || errors.Is(err, context.DeadlineExceeded) {
+		return err
+	}
+
+	return fmt.Errorf("%w: %w", err, core_postgres_pool.ErrUnknown)
+}
---
+++ internal/core/repository/postgres/pgxadapter/pool.go
@@ -0,0 +1,79 @@
+package pgxadapter
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	core_postgres_pool "github.com/daniildddd/maestro/internal/core/repository/postgres"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type Pool struct {
+	*pgxpool.Pool
+	opTimeout time.Duration
+}
+
+func NewPool(
+	ctx context.Context,
+	config Config,
+) (*Pool, error) {
+	pgxConfig, err := pgxpool.ParseConfig(config.DSN)
+	if err != nil {
+		return nil, fmt.Errorf("parse pgxconfig: %w", err)
+	}
+
+	pool, err := pgxpool.NewWithConfig(ctx, pgxConfig)
+	if err != nil {
+		return nil, fmt.Errorf("create pgxpool: %w", err)
+	}
+
+	if err := pool.Ping(ctx); err != nil {
+		return nil, fmt.Errorf("pgxpool ping: %w", err)
+	}
+
+	return &Pool{
+		Pool:      pool,
+		opTimeout: config.Timeout,
+	}, nil
+}
+
+func (p *Pool) Query(
+	ctx context.Context,
+	sql string,
+	args ...any,
+) (core_postgres_pool.Rows, error) {
+	rows, err := p.Pool.Query(ctx, sql, args...)
+	if err != nil {
+		return nil, mapErrors(err)
+	}
+
+	return pgxRows{rows}, nil
+}
+
+func (p *Pool) QueryRow(
+	ctx context.Context,
+	sql string,
+	args ...any,
+) core_postgres_pool.Row {
+	row := p.Pool.QueryRow(ctx, sql, args...)
+
+	return pgxRow{row}
+}
+
+func (p *Pool) Exec(
+	ctx context.Context,
+	sql string,
+	args ...any,
+) (core_postgres_pool.CommandTag, error) {
+	commandTag, err := p.Pool.Exec(ctx, sql, args...)
+	if err != nil {
+		return nil, mapErrors(err)
+	}
+
+	return pgxCommandTag{commandTag}, nil
+}
+
+func (p *Pool) OpTimeout() time.Duration {
+	return p.opTimeout
+}

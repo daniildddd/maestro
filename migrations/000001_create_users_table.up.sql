6542411665d5b849aab8dd84e69d094fe8e550af
Lyapunov Daniil <danyalyapunov@gmail.com>
2026-07-14 23:41:15 +0300

    feat(migrations): add create_users_table migration

---
+++ migrations/000001_create_users_table.up.sql
@@ -0,0 +1,10 @@
+CREATE TABLE users (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    username TEXT NOT NULL UNIQUE,
+    password_hash TEXT NOT NULL,
+    role TEXT NOT NULL ,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    updated_at TIMESTAMPTZ,
+
+    CONSTRAINT chk_users_role CHECK ( role IN ('admin','user','viewer') )
+)

0ba9ddfd86198fb5dfff976458c7db090d1cc40c
Lyapunov Daniil <danyalyapunov@gmail.com>
2026-07-16 21:11:15 +0300

    feat(auth): add POST /login handler with refresh token cookie

---
+++ internal/features/auth/transport/login.go
@@ -0,0 +1,78 @@
+package transport
+
+import (
+	"net/http"
+
+	"github.com/daniildddd/maestro/internal/core/domain"
+	"github.com/daniildddd/maestro/internal/core/logger"
+	"github.com/daniildddd/maestro/internal/core/transport/request"
+	core_http_response "github.com/daniildddd/maestro/internal/core/transport/response"
+)
+
+type LoginUserRequest struct {
+	Username string `json:"username" validate:"required,min=3,max=100"`
+	Password string `json:"password" validate:"required,min=6,max=30"`
+}
+
+type LoginUserResponse struct {
+	AccessToken string `json:"access_token"`
+	Username    string `json:"username"`
+}
+
+func (h *AuthHTTPHandler) login(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	log := logger.FromContext(ctx)
+	responseHandler := core_http_response.NewHTTPResponseHandler(w, log)
+
+	var loginRequest LoginUserRequest
+	if err := request.DecodeAndValidate(w, r, &loginRequest); err != nil {
+		responseHandler.ErrorResponse(
+			err,
+			"failed decode and validate HTTP request",
+		)
+
+		return
+	}
+
+	loginCredentials, err := h.authService.Login(
+		ctx,
+		loginRequest.Username,
+		loginRequest.Password,
+	)
+
+	if err != nil {
+		responseHandler.ErrorResponse(
+			err,
+			"failed to login",
+		)
+
+		return
+	}
+
+	http.SetCookie(w, &http.Cookie{
+		Name:     "refresh_token",
+		Value:    loginCredentials.RefreshToken,
+		Path:     "/",
+		HttpOnly: true,
+		Secure:   h.cfg.CookieSecure,
+		SameSite: http.SameSiteLaxMode,
+		Domain:   h.cfg.CookieDomain,
+		Expires:  loginCredentials.ExpiresAt,
+	})
+
+	response := loginResponseFromDomain(loginCredentials)
+
+	responseHandler.JSONResponse(
+		response,
+		http.StatusOK,
+	)
+}
+
+func loginResponseFromDomain(
+	credentials domain.TokenPair,
+) LoginUserResponse {
+	return LoginUserResponse{
+		AccessToken: credentials.AccessToken,
+		Username:    credentials.Username,
+	}
+}

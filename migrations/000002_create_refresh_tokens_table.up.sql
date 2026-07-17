b6ce226d5d0774471160f3bb28740bb4b2852bea
Lyapunov Daniil <danyalyapunov@gmail.com>
2026-07-17 03:16:53 +0300

    fix(migrations): rename expired_at to expires_at in refresh_tokens table

--- migrations/000002_create_refresh_tokens_table.up.sql
+++ migrations/000002_create_refresh_tokens_table.up.sql
@@ -3,6 +3,6 @@ CREATE TABLE refresh_tokens (
     user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE ,
     token_hash TEXT NOT NULL ,
     created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
-    expired_at TIMESTAMPTZ NOT NULL ,
+    expires_at TIMESTAMPTZ NOT NULL ,
     revoked_at TIMESTAMPTZ
 )

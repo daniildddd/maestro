105ae0c73ee724b5792d6eb1994ed51d3195c1b2
Lyapunov Daniil <danyalyapunov@gmail.com>
2026-07-20 16:45:55 +0300

    feat(middleware): add RequestID and Logger middlewares

---
+++ internal/core/transport/middleware/common.go
@@ -0,0 +1,44 @@
+package middleware
+
+import (
+	"net/http"
+
+	core_logger "github.com/daniildddd/maestro/internal/core/logger"
+	"github.com/google/uuid"
+	"go.uber.org/zap"
+)
+
+const requestIDHeader = "X-Request-ID"
+
+func RequestID() Middleware {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			requestID := r.Header.Get(requestIDHeader)
+			if requestID == "" {
+				requestID = uuid.New().String()
+			}
+
+			r.Header.Set(requestIDHeader, requestID)
+			w.Header().Set(requestIDHeader, requestID)
+
+			next.ServeHTTP(w, r)
+		})
+	}
+}
+
+func Logger(log *core_logger.Logger) Middleware {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			requestID := r.Header.Get(requestIDHeader)
+
+			l := log.With(
+				zap.String("request_id", requestID),
+				zap.String("url", r.URL.Path),
+			)
+
+			ctx := core_logger.ToContext(r.Context(), l)
+
+			next.ServeHTTP(w, r.WithContext(ctx))
+		})
+	}
+}

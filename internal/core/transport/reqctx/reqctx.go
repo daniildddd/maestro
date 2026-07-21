8b0f3353201d2e621a643195bf967f0eddae6a2e
Lyapunov Daniil <danyalyapunov@gmail.com>
2026-07-21 03:29:04 +0300

    feat(reqctx): add user id context helpers

--- internal/core/transport/reqctx/reqctx.go
+++ internal/core/transport/reqctx/reqctx.go
@@ -1,13 +1,18 @@
 package reqctx
 
-import "context"
+import (
+	"context"
+
+	"github.com/google/uuid"
+)
 
 type ctxKey struct {
 	name string
 }
 
 var (
-	keyRole = ctxKey{name: "role"}
+	keyRole   = ctxKey{name: "role"}
+	keyUserId = ctxKey{name: "user_id"}
 )
 
 func WithKeyRole(ctx context.Context, role string) context.Context {
@@ -16,10 +21,25 @@ func WithKeyRole(ctx context.Context, role string) context.Context {
 	return ctx
 }
 
+func WithUserId(ctx context.Context, userId uuid.UUID) context.Context {
+	ctx = context.WithValue(ctx, keyUserId, userId)
+
+	return ctx
+}
+
 func Role(ctx context.Context) string {
 	v, ok := ctx.Value(keyRole).(string)
 	if !ok {
 		panic("role not found in context")
+	}
+
+	return v
+}
+
+func UserId(ctx context.Context) uuid.UUID {
+	v, ok := ctx.Value(keyUserId).(uuid.UUID)
+	if !ok {
+		panic("userId not found in context")
 	}
 
 	return v

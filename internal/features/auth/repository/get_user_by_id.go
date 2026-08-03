dad53b06b960b51af9f4ef6026af389733fbc863
Lyapunov Daniil <danyalyapunov@gmail.com>
2026-08-03 19:07:50 +0300

    feat(auth/repository): add GetUserById

---
+++ internal/features/auth/repository/get_user_by_id.go
@@ -0,0 +1,49 @@
+package repository
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/daniildddd/maestro/internal/core/domain"
+	"github.com/daniildddd/maestro/internal/core/errs"
+	"github.com/daniildddd/maestro/internal/core/repository/postgres"
+	"github.com/google/uuid"
+)
+
+func (r *AuthRepository) GetUserById(
+	ctx context.Context,
+	id uuid.UUID,
+) (domain.User, error) {
+	const op = "auth.repository.GetById"
+
+	ctx, cancel := context.WithTimeout(ctx, r.pool.OpTimeout())
+	defer cancel()
+
+	query := `
+	SELECT id, username, password_hash, role, created_at, updated_at
+	FROM users
+	WHERE id=$1`
+	row := r.pool.QueryRow(ctx, query, id)
+
+	var dbUser userModel
+	if err := dbUser.Scan(row); err != nil {
+		if errors.Is(err, postgres.ErrNoRows) {
+			return domain.User{}, fmt.Errorf(
+				"%s: scan user(user_id='%s'): %w",
+				op,
+				id,
+				errs.ErrUserNotFound,
+			)
+		}
+
+		return domain.User{}, fmt.Errorf(
+			"%s: scan user(user_id='%s'): %w",
+			op,
+			id,
+			err,
+		)
+	}
+
+	return dbUser.toDomain(), nil
+}

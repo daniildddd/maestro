89dada7e49b3046ee0ef211f7d3fec8b70b4ef4c
Lyapunov Daniil <danyalyapunov@gmail.com>
2026-08-03 19:08:00 +0300

    feat(auth/service): add Logout method

---
+++ internal/features/auth/service/logout.go
@@ -0,0 +1,25 @@
+package service
+
+import (
+	"context"
+	"fmt"
+)
+
+func (s *AuthService) Logout(
+	ctx context.Context,
+	rawToken string,
+) error {
+	const op = "auth.service.Logout"
+	token := s.refreshGen.Hash(rawToken)
+
+	err := s.authRepository.DeleteRefreshToken(ctx, token)
+	if err != nil {
+		return fmt.Errorf(
+			"%s: delete refresh token: %w",
+			op,
+			err,
+		)
+	}
+
+	return nil
+}

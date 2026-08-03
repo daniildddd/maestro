e4109ae215e462cfb4c4efd72ceca73c1baa9abb
Lyapunov Daniil <danyalyapunov@gmail.com>
2026-08-03 19:07:13 +0300

    refactor(response): rename ResponseWriter to RWriter

--- internal/core/transport/response/writer.go
+++ internal/core/transport/response/writer.go
@@ -6,24 +6,24 @@ var (
 	StatusCodeUninitialized = -1
 )
 
-type ResponseWriter struct {
+type RWriter struct {
 	http.ResponseWriter
 	statusCode int
 }
 
-func NewResponseWriter(w http.ResponseWriter) *ResponseWriter {
-	return &ResponseWriter{
+func NewResponseWriter(w http.ResponseWriter) *RWriter {
+	return &RWriter{
 		ResponseWriter: w,
 		statusCode:     StatusCodeUninitialized,
 	}
 }
 
-func (rw *ResponseWriter) WriteHeader(statusCode int) {
+func (rw *RWriter) WriteHeader(statusCode int) {
 	rw.ResponseWriter.WriteHeader(statusCode)
 	rw.statusCode = statusCode
 }
 
-func (rw *ResponseWriter) GetStatusCode() int {
+func (rw *RWriter) GetStatusCode() int {
 	if rw.statusCode == StatusCodeUninitialized {
 		return http.StatusOK
 	}

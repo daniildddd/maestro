3a89f951f084ce2b179da18447998af44f8996e3
Lyapunov Daniil <danyalyapunov@gmail.com>
2026-08-03 19:07:48 +0300

    feat(auth/repository): add GetRefreshTokenByHash

---
+++ internal/features/auth/repository/get_refresh_token.go
@@ -0,0 +1,47 @@
+package repository
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/daniildddd/maestro/internal/core/domain"
+	"github.com/daniildddd/maestro/internal/core/errs"
+	"github.com/daniildddd/maestro/internal/core/repository/postgres"
+)
+
+func (r *AuthRepository) GetRefreshTokenByHash(
+	ctx context.Context,
+	tokenHash string,
+) (domain.RefreshToken, error) {
+	const op = "auth.repository.GetRefreshTokenByHash"
+
+	ctx, cancel := context.WithTimeout(ctx, r.pool.OpTimeout())
+	defer cancel()
+
+	query := `
+	SELECT id, user_id, token_hash, created_at, expires_at
+	FROM refresh_tokens
+	WHERE token_hash=$1`
+
+	row := r.pool.QueryRow(ctx, query, tokenHash)
+
+	var dbToken refreshTokenModel
+	if err := dbToken.Scan(row); err != nil {
+		if errors.Is(err, postgres.ErrNoRows) {
+			return domain.RefreshToken{}, fmt.Errorf(
+				"%s: scan row: %w",
+				op,
+				errs.ErrRefreshTokenNotFound,
+			)
+		}
+
+		return domain.RefreshToken{}, fmt.Errorf(
+			"%s: scan row: %w",
+			op,
+			err,
+		)
+	}
+
+	return dbToken.toDomain(), nil
+}

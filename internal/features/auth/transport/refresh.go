d8c3041fcd52f7d4c25d9a95a29ea7ae340a68a1
Lyapunov Daniil <danyalyapunov@gmail.com>
2026-08-03 19:08:15 +0300

    feat(auth/transport): add refresh HTTP handler

---
+++ internal/features/auth/transport/refresh.go
@@ -0,0 +1,69 @@
+package transport
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/daniildddd/maestro/internal/core/errs"
+	"github.com/daniildddd/maestro/internal/core/logger"
+	"github.com/daniildddd/maestro/internal/core/transport/response"
+)
+
+type refreshResponse struct {
+	AccessToken string `json:"access_token"`
+}
+
+func (h *AuthHTTPHandler) refresh(w http.ResponseWriter, r *http.Request) {
+	const op = "auth.transport.refresh"
+
+	ctx := r.Context()
+	log := logger.FromContext(ctx)
+	responseHandler := response.NewHTTPResponseHandler(w, log)
+	cookie, err := r.Cookie("refresh_token")
+	if err != nil {
+		responseHandler.ErrorResponse(
+			fmt.Errorf(
+				"%s: missing refresh token cookie: %w",
+				op,
+				errs.ErrMissingRefreshToken,
+			),
+		)
+
+		return
+	}
+
+	refreshToken := cookie.Value
+
+	tokenPair, err := h.authService.Refresh(ctx, refreshToken)
+	if err != nil {
+		responseHandler.ErrorResponse(
+			fmt.Errorf(
+				"%s: refresh tokens: %w",
+				op,
+				err,
+			),
+		)
+
+		return
+	}
+
+	http.SetCookie(w, &http.Cookie{
+		Name:     "refresh_token",
+		Value:    tokenPair.RefreshToken,
+		Path:     "/",
+		Domain:   h.cfg.CookieDomain,
+		Expires:  tokenPair.ExpiresAt,
+		Secure:   h.cfg.CookieSecure,
+		HttpOnly: true,
+		SameSite: http.SameSiteLaxMode,
+	})
+
+	refreshResp := refreshResponse{
+		AccessToken: tokenPair.AccessToken,
+	}
+
+	responseHandler.JSONResponse(
+		refreshResp,
+		http.StatusOK,
+	)
+}

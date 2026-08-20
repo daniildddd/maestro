dce4c5712ab869e15c1ef6a472ee748c813c5a8c
Lyapunov Daniil <danyalyapunov@gmail.com>
2026-08-20 17:54:54 +0300

    refactor(users/transport): move response mapping to shared file

--- internal/features/users/transport/get_users.go
+++ internal/features/users/transport/get_users.go
@@ -102,23 +102,3 @@ func (h *UsersHTTPHandler) GetUsers(w http.ResponseWriter, r *http.Request) {
 		http.StatusOK,
 	)
 }
-
-func usersResponseFromDomain(users []domain.User) []UserResponse {
-	resp := make([]UserResponse, 0, len(users))
-
-	for _, u := range users {
-		resp = append(resp, userResponseFromDomain(u))
-	}
-
-	return resp
-}
-
-func userResponseFromDomain(u domain.User) UserResponse {
-	return UserResponse{
-		ID:        u.ID.String(),
-		Username:  u.Username,
-		Role:      u.Role,
-		CreatedAt: u.CreatedAt,
-		UpdatedAt: u.UpdatedAt,
-	}
-}

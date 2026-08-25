5be80546a70014ff4f97e4837977352c3e95e03e
Lyapunov Daniil <danyalyapunov@gmail.com>
2026-08-25 04:57:12 +0300

    feat(connectors): add kafka connect client config

---
+++ internal/features/connectors/kafkaconnect/config.go
@@ -0,0 +1,23 @@
+package kafkaconnect
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/kelseyhightower/envconfig"
+)
+
+type Config struct {
+	BaseURL string        `envconfig:"BASE_URL" required:"true"`
+	Timeout time.Duration `default:"5s"         envconfig:"TIMEOUT"`
+}
+
+func NewConfigMust() Config {
+	var cfg Config
+
+	if err := envconfig.Process("KAFKA_CONNECT", &cfg); err != nil {
+		panic(fmt.Errorf("process kafka connect config: %w", err))
+	}
+
+	return cfg
+}
